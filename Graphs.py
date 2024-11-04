3a35b8453b37c4fb8dde72ca4c162a0f0a596a5d
jonwynn <143842244+jonwynn@users.noreply.github.com>
2024-11-04 12:26:52 -0600

    Add files via upload

--- Graphs.py
+++ Graphs.py
@@ -3,25 +3,6 @@ import collections
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# sample code to create and display graph via matplotlib
-# G = nx.Graph()
-#
-# # add nodes to the graph
-# G.add_node("A")
-# G.add_node("B")
-# G.add_node("C")
-# G.add_node("D")
-
-# # connect nodes with edges
-# G.add_edge("A", "B")
-# G.add_edge("A", "C")
-# G.add_edge("B", "D")
-# G.add_edge("C", "D")
-
-# # draw the graph
-# nx.draw(G, with_labels=True, node_color='lightblue', edge_color='red', node_size=1000, font_size=15)
-# plt.show()
-
 class Graph1:
     """
     You are asked to represent the undirected graph below in a computer. Then, apply it
@@ -132,11 +113,113 @@ class Graph2:
     project report (Note: you don’t have to draw it with a program. Draw it manually
     in your report is acceptable); and then
     c) Represent the ‘meta graph’ as a DAG and linearize it in its topological order.
+    
+    nodes2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    
+    edges2 = [
+        ('1', '3'), ('2', '1'), ('3', '2'), ('3', '5'), ('4', '1'),
+        ('4', '2'), ('4', '12'), ('5', '6'), ('5', '8'), ('6', '7'),
+        ('6', '8'), ('6', '10'), ('7', '10'), ('8', '9'), ('8', '10'), 
+        ('9', '5'), ('9', '11'), ('10', '9'), ('10', '11'), ('11', '12')
+    ]
+    
     """
+    def __init__(self): # Constructor
+        # Initialize graph
+        self.G = nx.DiGraph()
 
-    pass # Delete and replace with implementation
+        # Create nodes
+        nodes2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+        self.G.add_nodes_from(nodes2)
 
+        # Add edges
+        edges2 = [
+            ('1', '3'), ('3', '2'), ('2', '1'), ('3', '5'), ('4', '1'),
+            ('4', '2'), ('4', '12'), ('5', '6'), ('5', '8'), ('6', '7'),
+            ('6', '8'), ('6', '10'), ('7', '10'), ('8', '9'), ('8', '10'), 
+            ('9', '5'), ('9', '11'), ('10', '9'), ('10', '11'), ('11', '12')
+        ]
+
+        self.G.add_edges_from(edges2)
+
+    def displayGraph2(self):
+        plt.figure(figsize=(10, 8))
+        pos = nx.spring_layout(self.G)
+
+        # Draw graph
+        nx.draw_networkx_nodes(self.G, pos)
+        nx.draw_networkx_labels(self.G, pos)
+        nx.draw_networkx_edges(self.G, pos, edge_color='r', arrows = True)
+
+        plt.show()
+        
+    """
+    https://www.geeksforgeeks.org/strongly-connected-components/
+    """
+    # dfs Function to reach destination
+    def dfs2(self, curr, des, adj, vis):
+        # If current node is the destination, return True
+        if curr == des:
+            return True
+        vis[curr] = 1
+        for x in adj[curr]:
+            if not vis[x]:
+                if self.dfs2(x, des, adj, vis):
+                    return True
+        return False
+    
+    # To tell whether there is a path from source to destination
+    def isPath2(self, src, des, adj):
+        vis = [0] * (len(adj) + 1)
+        return self.dfs2(src, des, adj, vis)
+    
+    # Function to return all the strongly connected components of a graph.
+    def findSCC(self, n, a):
+        # Stores all the strongly connected components.
+        ans = []
+        
+        # Stores whether a vertex is a part of any Strongly Connected Component
+        is_scc = [0] * (n + 1)
+        
+        adj = [[] for _ in range(n + 1)]
+        
+        for i in range(len(a)):
+            adj[a[i][int(0)]].append(a[i][int(1)])
+        
+        # Traversing all the vertices
+        for i in range(1, n + 1):
+            if not is_scc[i]:
+                # If a vertex i is not a part of any SCC, insert it into a new SCC list
+                # and check for other vertices whether they can be part of this list.
+                scc = [i]
+                for j in range(i + 1, n + 1):
+                    # If there is a path from vertex i to vertex j and vice versa,
+                    # put vertex j into the current SCC list.
+                    if not is_scc[j] and self.isPath2(i, j, adj) and self.isPath2(j, i, adj):
+                        is_scc[j] = 1
+                        scc.append(j)
+                # Insert the SCC containing vertex i into the final list.
+                ans.append(scc)
+        return ans
+
+    def sccTest(self):
+        obj = Graph2()
+        V = int(12)
+        sccEdge = [
+            [1, 3], [3, 2], [2, 1], [3, 5], [5, 6],
+            [4, 2], [4, 1], [4, 12], [5, 8], [9, 5],
+            [6, 7], [6, 8], [6, 10], [7, 10], [8, 9],
+            [8, 10], [9, 11], [10, 9], [10, 11], [11, 12]
+        ]
+        ans = obj.findSCC(V, sccEdge)
+        print("Strongly Connected Components are:")
+        for x in ans:
+            for y in x:
+                print(y, end=" ")
+            print()
+        
 class Graph3:
+    
     """
     Provided is a weighted undirected graph. Implement it as a graph object and then
     computationally answer the following questions:
@@ -148,8 +231,8 @@ class Graph3:
     c) Are a shortest path tree and a minimum spanning tree usually the same?
     d) If the graph has an edge with a negative weight, can you apply Dijkstra’s
     algorithm to find a shortest path tree?
-    """
 
+    """
     pass # Delete and replace with implementation
 
 def main():
@@ -180,7 +263,8 @@ def main():
             graph1.bfsTest()
             continue
         elif selection == '2':
-            # graph2.displayGraph()
+            graph2.displayGraph2()
+            graph2.sccTest()
             continue
         elif selection == '3':
             # graph3.displayGraph()
